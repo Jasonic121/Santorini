400903cc4f3445d5457727e82639a8f63a4e4c5a
jasonli5 <jasonli5@andrew.cmu.edu>
2024-03-19 23:02:32 -0400

    Fixed extra semicolin in Worker

--- santorini-boardgame/src/main/java/com/santorini/java/Worker.java
+++ santorini-boardgame/src/main/java/com/santorini/java/Worker.java
@@ -51,7 +51,7 @@ public class Worker {
             this.currentCell.setOccupied(false);
             destination.setOccupied(true);
             this.currentCell = destination;
-        };
+        }
     }
 
     /**
