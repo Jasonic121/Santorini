76f44d3f32e2c44e18ad84cd714619f0c8634db1
jasonli5 <jasonli5@andrew.cmu.edu>
2024-04-06 11:39:07 -0400

    Made build and move selection boxes different colors

--- frontend/src/Cell.tsx
+++ frontend/src/Cell.tsx
@@ -22,14 +22,16 @@ class BoardCell extends React.Component<Props> {
     const height = `height-${cell.height}`;
 
     const isSelectedWorker = selectedWorkerCell?.x === cell.x && selectedWorkerCell?.y === cell.y;
-    const isValidMove = validMoves?.some(move => move.x === cell.x && move.y === cell.y);
+    let isValidMove = validMoves?.some(move => move.x === cell.x && move.y === cell.y);
+    const isValidBuild = isValidMove && currentWorkerPhase === 1;
+    isValidMove = isValidMove && currentWorkerPhase === 0;
     const hasSelectedWorker = selectedWorkerCell !== null;
     const playerAlt = cell.occupiedBy === 0 ? 'Player 1' : 'Player 2';
     const playerImage = cell.occupiedBy === 0 ? player1 : player2;
     const isCurrentPlayerWorker = this.props.isCurrentPlayerWorker && cell.occupied && Number(cell.occupiedBy) === this.props.currentPlayer;
 
     return (
-      <div className={`cell ${occupied} ${dome} ${height} ${isSelectedWorker ? 'selected-worker' : ''} ${isValidMove ? 'valid-move' : ''}`}>
+      <div className={`cell ${occupied} ${dome} ${height} ${isSelectedWorker ? 'selected-worker' : ''} ${isValidMove ? 'valid-move' : ''} ${isValidBuild ? 'valid-build' : ''}`}>
         {cell.occupied && (
         <div className={`worker player${cell.occupiedBy} ${ (currentGamePhase!=1) && (!currentWorkerPhase) && isCurrentPlayerWorker && (!hasSelectedWorker) ? 'currentPlayerWorker' : ''}`}>
             <img src={playerImage} alt={playerAlt} />
